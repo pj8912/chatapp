4abbb9de398f9b720f1cab787f4dc961202369ac
pj8912 <jpintofun2b@gmail.com>
2022-12-19 21:41:10 +0530

    commented unwanted code

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -8,11 +8,11 @@ class Config{
 
     static $home = "http://localhost/chatapp";
 
-    public static function get_url(){
-        return 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];   
-    }   
+    // public static function get_url(){
+    //     return 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];   
+    // }   
 
-    public static function get_uri(){
-        return $_SERVER['REQUEST_URI'];
-    }
+    // public static function get_uri(){
+    //     return $_SERVER['REQUEST_URI'];
+    // }
 }
